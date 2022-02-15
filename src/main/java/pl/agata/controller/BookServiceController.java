d593ff58d72346746f9e08dcc6dd5a0dd3168b0a
Rashi <agata.rachanska.programista@gmail.com>
2022-02-15 16:38:01 +0100

    Add search pages and empty search method

--- src/main/java/pl/agata/controller/BookServiceController.java
+++ src/main/java/pl/agata/controller/BookServiceController.java
@@ -14,7 +14,6 @@ import pl.agata.service.BasketService;
 import pl.agata.service.BookService;
 import pl.agata.user.User;
 
-
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,6 +25,7 @@ public class BookServiceController {
     private BookConfig bookConfig;
     private BasketConfig basketConfig;
     private List<Book> books;
+    private List<Book> allBooks;
     private User user;
     private String title;
     private String author;
@@ -47,38 +47,145 @@ public class BookServiceController {
     @GetMapping("/index")
     public String showMainPage(Book book, User user, Model model) throws SQLException {
         this.books = new ArrayList<>();
+        this.allBooks = new ArrayList<>();
         BookService bookService =  bookConfig.getBookService();
         books.clear();
-        this.books = bookService.addBook();
-        model.addAttribute("books",this.books);
+
+        int pagesBooks = bookService.quantityPagesAllBook();
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        allBooks.clear();
+        HashMap<String,Object> maps = new HashMap<>();
+        String urlSearchBook = "/index/pages=";
+        this.books = bookService.getBooksLimit(0);
+        maps.put("books",this.books);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
+        model.addAllAttributes(maps);
         return "index";
     }
 
-    @GetMapping("/loginUsersIndex")
-    public String showLoginMainPage(Book book,@SessionAttribute("loginUser") User user, Model model) throws SQLException {
-        books = new ArrayList<>();
+    @GetMapping("/index/pages={numberPage}")
+    public String showPage(Book book, User user, Model model,@PathVariable int numberPage) throws SQLException {
+        this.books = new ArrayList<>();
+        this.allBooks = new ArrayList<>();
+        BookService bookService =  bookConfig.getBookService();
+        books.clear();
+
+        int pagesBooks = bookService.quantityPagesAllBook();
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        allBooks.clear();
         HashMap<String,Object> maps = new HashMap<>();
+        String urlSearchBook = "/index/pages=";
+        this.books = bookService.getBooksLimit(numberPage-1);
+        maps.put("books",this.books);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
+        model.addAllAttributes(maps);
+        return "index";
+    }
+
+    @GetMapping("/loginUsersIndex/pages={numberPage}")
+    public String showLoginMainPage(Book book,@SessionAttribute("loginUser") User user, Model model,@PathVariable int numberPage) throws SQLException {
+        this.books = new ArrayList<>();
+        this.allBooks = new ArrayList<>();
         BookService bookService =  bookConfig.getBookService();
         BasketService basketService = new BasketService(bookConfig.getDataService().getDBService());
-        this.books = bookService.addBook();
+        books.clear();
+
+        int pagesBooks = bookService.addBook().size()%12+1;
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        allBooks.clear();
         basketBooksQuantity = basketService.getBasketSize(user.getId());
+        HashMap<String,Object> maps = new HashMap<>();
+        String urlSearchBook = "/loginUsersIndex/pages=";
+        this.books = bookService.getBooksLimit(numberPage-1);
         maps.put("user",user);
         maps.put("books",this.books);
         maps.put("basketQuantity", basketBooksQuantity);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
+        model.addAllAttributes(maps);
+        return "loginUsersIndex";
+    }
+
+    @GetMapping("/loginUsersIndex")
+    public String showLoginPage(Book book,@SessionAttribute("loginUser") User user, Model model) throws SQLException {
+        this.books = new ArrayList<>();
+        this.allBooks = new ArrayList<>();
+        BookService bookService =  bookConfig.getBookService();
+        BasketService basketService = new BasketService(bookConfig.getDataService().getDBService());
+        books.clear();
+
+        int pagesBooks = bookService.quantityPagesAllBook();
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        allBooks.clear();
+        basketBooksQuantity = basketService.getBasketSize(user.getId());
+        HashMap<String,Object> maps = new HashMap<>();
+        String urlSearchBook = "/loginUsersIndex/pages=";
+        this.books = bookService.getBooksLimit(0);
+        maps.put("user",user);
+        maps.put("books",this.books);
+        maps.put("basketQuantity", basketBooksQuantity);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
         model.addAllAttributes(maps);
         return "loginUsersIndex";
     }
 
     @GetMapping("/index/search/{title}")
-    public String showSearchPage(Book book, User user, Model model) throws SQLException {
+    public String showSearchPage(Book book, User user, Model model, @PathVariable String title) throws SQLException {
         this.books.clear();
         this.books = new ArrayList<>();
-
         BookService bookService =  bookConfig.getBookService();
-        System.out.println(book.getTitle());
-        this.books = bookService.searchBook(book.getTitle());
-        System.out.println(books.size());
-        model.addAttribute("books",this.books);
+        int pagesBooks = bookService.quantityPagesSearchBook(book.getTitle());
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        this.books = bookService.searchBook(book.getTitle(),0);
+        String urlSearchBook = "/index/search/"+title+"/pages=";
+        HashMap<String,Object> maps = new HashMap<>();
+        maps.put("books",this.books);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
+        model.addAllAttributes(maps);
+        return "index";
+    }
+    @GetMapping("/index/search/{title}/pages={numberPage}")
+    public String showSearchNumberPage(Book book, User user, Model model, @PathVariable String title,@PathVariable int numberPage) throws SQLException {
+        this.books.clear();
+        this.books = new ArrayList<>();
+        BookService bookService =  bookConfig.getBookService();
+        int pagesBooks = bookService.quantityPagesSearchBook(book.getTitle());
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        this.books = bookService.searchBook(book.getTitle(),numberPage-1);
+        String urlSearchBook = "/index/search/"+title+"/pages=";
+        HashMap<String,Object> maps = new HashMap<>();
+        maps.put("books",this.books);
+        maps.put("quantityPages", quantityPages);
+        maps.put("urlSearchBook",urlSearchBook);
+        model.addAllAttributes(maps);
         return "index";
     }
 
@@ -86,22 +193,59 @@ public class BookServiceController {
     public String searchBook(Book book, User user, Model model,BindingResult result) throws SQLException {
         BookService bookService =  bookConfig.getBookService();
         String title = book.getTitle();
-        this.books= bookService.searchBook(title);
-        return "redirect:/index/search/"+title;
+        this.books= bookService.searchBook(title,0);
+        if(title!="") {
+            return "redirect:/index/search/" + title;
+        } else {
+            return "redirect:/index";
+        }
     }
 
     @GetMapping("/loginUsersIndex/search/{title}")
-    public String showLoginUserSearchPage(Book book, @SessionAttribute("loginUser")User user, Model model) throws SQLException {
+    public String showLoginUserSearchPage(Book book, @SessionAttribute("loginUser")User user, Model model,@PathVariable String title) throws SQLException {
         this.books.clear();
         this.books = new ArrayList<>();
         HashMap<String,Object> maps = new HashMap<>();
         BookService bookService =  bookConfig.getBookService();
-        this.books = bookService.searchBook(book.getTitle());
+        this.books = bookService.searchBook(book.getTitle(),0);
         BasketService basketService = new BasketService(bookConfig.getDataService().getDBService());
         basketBooksQuantity = basketService.getBasketSize(user.getId());
+        int pagesBooks = bookService.quantityPagesSearchBook(book.getTitle());
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        String urlSearchBook = "/loginUsersIndex/search/"+title+"/pages=";
         maps.put("user",user);
         maps.put("books",this.books);
         maps.put("basketQuantity", basketBooksQuantity);
+        maps.put("urlSearchBook",urlSearchBook);
+        maps.put("quantityPages", quantityPages);
+        model.addAllAttributes(maps);
+        return "loginUsersIndex";
+    }
+    @GetMapping("/loginUsersIndex/search/{title}/pages={numberPage}")
+    public String showLoginUserSearchPageNumber(Book book, @SessionAttribute("loginUser")User user, Model model,@PathVariable String title,@PathVariable int numberPage) throws SQLException {
+        this.books.clear();
+        this.books = new ArrayList<>();
+        HashMap<String,Object> maps = new HashMap<>();
+        BookService bookService =  bookConfig.getBookService();
+        this.books = bookService.searchBook(book.getTitle(),numberPage-1);
+        BasketService basketService = new BasketService(bookConfig.getDataService().getDBService());
+        basketBooksQuantity = basketService.getBasketSize(user.getId());
+        int pagesBooks = bookService.quantityPagesSearchBook(book.getTitle());
+        int[] quantityPages = new int[pagesBooks];
+        for(int i = 0; i<pagesBooks;i++)
+        {
+            quantityPages[i] = i;
+        }
+        String urlSearchBook = "/loginUsersIndex/search/"+title+"/pages=";
+        maps.put("user",user);
+        maps.put("books",this.books);
+        maps.put("basketQuantity", basketBooksQuantity);
+        maps.put("urlSearchBook",urlSearchBook);
+        maps.put("quantityPages", quantityPages);
         model.addAllAttributes(maps);
         return "loginUsersIndex";
     }
@@ -110,8 +254,12 @@ public class BookServiceController {
     public String searchUserLoginBook(Book book, @SessionAttribute("loginUser")User user, Model model,BindingResult result) throws SQLException {
         BookService bookService =  bookConfig.getBookService();
         String title = book.getTitle();
-        this.books= bookService.searchBook(title);
-        return "redirect:/loginUsersIndex/search/"+title;
+        this.books= bookService.searchBook(title,0);
+        if(title!="") {
+            return "redirect:/loginUsersIndex/search/" + title;
+        } else {
+            return "redirect:/loginUsersIndex";
+        }
     }
 
     @ExceptionHandler(ServletRequestBindingException.class)
